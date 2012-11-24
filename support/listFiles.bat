9941b0353781e012b8356a66abee03400418cad9
noryb009 <noryb009@hotmail.com>
2012-11-24 13:06:03 -0500

    Added puppyFiles folder

--- support/listFiles.bat
+++ support/listFiles.bat
@@ -1,15 +1,15 @@
 @echo off
 cd puppyfiles
-echo Function InstallFiles > ..\src\fileList.nsh
+echo Function InstallFiles> ..\src\fileList.nsh
 for %%G in (*) DO (
-  echo File "puppyFiles\%%G" >> ..\src\fileList.nsh
+  if not .%%G==..gitignore echo File "puppyFiles\%%G">> ..\src\fileList.nsh
 )
-echo FunctionEnd >> ..\src\fileList.nsh
+echo FunctionEnd>> ..\src\fileList.nsh
 
-echo Function un.unInstallFiles >> ..\src\fileList.nsh
+echo Function un.unInstallFiles>> ..\src\fileList.nsh
 for %%G in (*) DO (
-  echo Delete "$INSTDIR\%%G" >> ..\src\fileList.nsh
+  if not .%%G==..gitignore echo Delete "$INSTDIR\%%G">> ..\src\fileList.nsh
 )
-echo FunctionEnd >> ..\src\fileList.nsh
+echo FunctionEnd>> ..\src\fileList.nsh
 
 cd ..
