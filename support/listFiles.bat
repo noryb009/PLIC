bea2c50998101a3135b255774d154add0a644598
noryb009 <noryb009@gmail.com>
2015-03-01 17:00:38 -0500

    Allow building on Linux
    Convert listFiles.bat to a bash/batch hybrid; allowing building on Linux

--- support/listFiles.bat
+++ support/listFiles.bat
@@ -1,3 +1,18 @@
+#|| goto :batch
+#!/bin/bash
+cd puppyFiles #
+echo "Function InstallFiles" > ../src/fileList.nsh #
+for i in *; do [ "x$i" != "x.gitignore" ] && echo "File \"puppyFiles/$i\"" >> ../src/fileList.nsh; done #
+echo "FunctionEnd" >> ../src/fileList.nsh #
+ #
+echo "Function un.unInstallFiles" >> ../src/fileList.nsh #
+for i in *; do [ "x$i" != "x.gitignore" ] && echo "Delete \"puppyFiles/$i\"" >> ../src/fileList.nsh; done #
+echo "FunctionEnd" >> ../src/fileList.nsh #
+ #
+cd .. #
+exit #
+||#
+:batch
 @echo off
 cd puppyfiles
 echo Function InstallFiles> ..\src\fileList.nsh
